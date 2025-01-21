dd314f49ab4e3f697b358e81f7e5360c0159afa9
ashishgoel1604 <ashish.goel1604@gmail.com>
2025-01-22 01:53:21 +0530

    updated test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Say Bye to React link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Say Bye to React/i);
   expect(linkElement).toBeInTheDocument();
 });
